7555b3b6c2eb903b75d49baae69f237573658d79
Emasslight2 <shabdan.asker@gmail.com>
2019-02-09 21:29:16 +0600

    lang labels added

--- Project 2/main.js
+++ Project 2/main.js
@@ -1,26 +1,16 @@
-let header = new Vue({
-    el: 'header',
+new Vue({
+    el: '#app',
     data: {
         nice:"WE've got the talent",
         niceRu:"У нас есть таланты",
         ru:true,
+        images:[
+          {image:'./images/Branding.png'},
+          {image:'./images/Branding_2.png'},
+          {image:'./images/Mobile_UI.png'},
+          {image:'./images/Web_design_2.png'},
+          {image:'./images/Illustrations.png'},
+          {image:'./images/Web_design.png'},
+        ]
     }
-  });
-let main = new Vue({
-  el: 'main',
-  data:{
-    images:[
-      {image:'./images/Branding.png'},
-      {image:'./images/Branding_2.png'},
-      {image:'./images/Mobile_UI.png'},
-      {image:'./images/Web_design_2.png'},
-      {image:'./images/Illustrations.png'},
-      {image:'./images/Web_design.png'},
-    ]
-  },
-  computed:{
-    filteredImages(image, index){
-      image=this.images[index];
-    }
-  }
 });
