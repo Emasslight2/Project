09248d9448cd3d6fc7efa8c195fdd954f117a5f9
Emasslight2 <shabdan.asker@gmail.com>
2019-02-09 22:13:33 +0600

    header translated

--- Project 2/main.js
+++ Project 2/main.js
@@ -1,9 +1,19 @@
 new Vue({
     el: '#app',
     data: {
-        nice:"WE've got the talent",
-        niceRu:"У нас есть таланты",
-        ru:true,
+        text_eng:{
+          header:{
+            header_h1:"We've got the talent",
+            header_text:"Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
+          }
+        },
+        text_ru:{
+          header:{
+            header_h1:"У нас есть таланты",
+            header_text:"Идейные соображения высшего порядка, а также постоянный количественный рост. Место обучения кадров влечет за собой процесс внедрения."
+          }
+        },
+        ru:false,
         images:[
           {image:'./images/Branding.png'},
           {image:'./images/Branding_2.png'},
