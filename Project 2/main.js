753dabe22bab803469b6ebe555c938ce62eb14a9
Emasslight2 <shabdan.asker@gmail.com>
2019-02-09 20:51:49 +0600

    images for filter added

--- Project 2/main.js
+++ Project 2/main.js
@@ -1,4 +1,4 @@
-let app = new Vue({
+let header = new Vue({
     el: 'header',
     data: {
         nice:"WE've got the talent",
@@ -6,3 +6,21 @@ let app = new Vue({
         ru:true,
     }
   });
+let main = new Vue({
+  el: 'main',
+  data:{
+    images:[
+      {image:'./images/Branding.png'},
+      {image:'./images/Branding_2.png'},
+      {image:'./images/Mobile_UI.png'},
+      {image:'./images/Web_design_2.png'},
+      {image:'./images/Illustrations.png'},
+      {image:'./images/Web_design.png'},
+    ]
+  },
+  computed:{
+    filteredImages(image, index){
+      image=this.images[index];
+    }
+  }
+});
