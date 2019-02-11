05c9ccccbf7b6f00c809551615b3ab89e1e94015
Emasslight2 <shabdan.asker@gmail.com>
2019-02-12 01:49:51 +0600

    added some images

--- Project 2/main.js
+++ Project 2/main.js
@@ -34,6 +34,13 @@ new Vue({
           {id: 4, image:'./images/Web_design_2.png', tags:['all', 'web_design'] },
           {id: 5, image:'./images/Illustrations.png', tags:['all', 'illustrations'] },
           {id: 6, image:'./images/Web_design.png', tags:['all', 'web_design'] },
+          {id: 7, image:'Creative_b.jpg', tags:['all', 'branding'] },
+          {id: 8, image:'ADK_brand.png', tags:['all', 'branding'] },
+          {id: 9, image:'Blackberry_desing.png', tags:['all', 'illustrations'] },
+          {id: 10, image:'consulting_mobile.jpg', tags:['all', 'mobile_ui'] },
+          {id: 11, image:'phone.png', tags:['all', 'mobile_ui'] },
+          {id: 12, image:'alamy_phone.jpg', tags:['all', 'mobile_ui'] },
+          {id: 13, image:'web.png', tags:['all', 'web_design'] },
         ],
         currentTag: 'all'
     },
@@ -42,7 +49,7 @@ new Vue({
         var filter = this.currentTag;
         return this.images.filter(function(img){
           return img.tags.indexOf(filter) !== -1;
-        });
+        }).slice(0,6);
       }
     },
     methods:{
